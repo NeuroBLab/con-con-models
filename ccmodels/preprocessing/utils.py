9e29303f07789c5bd14a638d4ba5b3c70db04062
Jacopo Biggiogera <jacopo.biggiogera@yahoo.it>
2024-03-19 13:25:10 +0100

    Add tuning_labler and osi_calculator functions

--- ccmodels/preprocessing/utils.py
+++ ccmodels/preprocessing/utils.py
@@ -1,5 +1,6 @@
 #Imports
 import numpy as np
+import pandas as pd
 
 def min_act(max_rad, model_type, dirs):
     '''This function returns the oreintation for where the minimum of the selective activity should be
@@ -138,6 +139,89 @@ def layer_extractor(input_df, transform, column = 'pre_pt_position'):
     input_df['cortex_layer'] = layers   
     return input_df
 
+
+def tuning_labler(df, id_col = 'root_id', delt_r_col = 'r_squared_diff', pval_col = 'pvalue', model_col = 'model_type'):
+    '''This function labels the neurons according to their tuning type. It labels neurons as 'not_selective' if they are not.
+    Args:
+    df: pandas dataframe containing the results of the tuning curve fitting
+    id_col: string, column name containing the unique id of each neuron
+    delt_r_col: string, column name containing the difference in r squared between the two models used during fitting process
+    pval_col: string, column name containing the p value of the wilcoxon test
+    model_col: string, column name containing the type of model used for fitting the tuning curve
+    
+    Returns:
+    neur_seltype: pandas dataframe containing the unique id of each neuron, and the type of tuning it displays
+    '''
+    
+    ############# Select all cells that are NOT selective ###############
+    #select all those with p value larger than 0.05
+    not_sel = df[df[pval_col]>0.05]
+    #group by root id and select ids of only those cells that are not significant for both orientation and direction
+    not_sel_grouped = not_sel.groupby(id_col).count().reset_index()
+    not_sel_id = not_sel_grouped[not_sel_grouped[pval_col]>1][id_col]
+
+    #Sleect only those cells that are not significant to both orientation and direction
+    not_sel = not_sel[not_sel[id_col].isin(not_sel_id)]
+
+    #Drop duplicates, so the fact that there are two entries for each cell
+    not_sel = not_sel.drop_duplicates(subset=id_col)
+    not_sel['tuning_type'] = not_sel[model_col].replace('single', 'not_selective')
+
+
+    ############# Select all cells that ARE selective ###############
+    good = df[df[pval_col]<0.05]
+
+    #Select all cells that are significant according to both models
+    grouped_res = good.groupby([id_col]).count().reset_index()
+    double_sig = grouped_res[grouped_res[pval_col]>1][id_col].values
+
+    #Select only the double model for those with 'fringe case 1'
+    double_fringe = good[(good[id_col].isin(double_sig)) & (good[delt_r_col]>0.8) & (good[model_col] == 'double')]
+
+    #Select only single model in neurons where both models significant and not in 'fringe case 1'
+    single_good = good[(good[id_col].isin(double_sig)) & (good[delt_r_col]<0.8) & (good[model_col] == 'single')]
+
+    #Select all remaining neurons with only one significant model
+    remaining_good = good[~good[id_col].isin(double_sig)]
+
+    tot_good = pd.concat([double_fringe,single_good,remaining_good]) 
+    tot_good['tuning_type'] = tot_good[model_col].replace('single', 'direction')
+    tot_good['tuning_type'] = tot_good[model_col].replace('double', 'orientation')
+    
+
+    #Concatenate selective and non selective cells
+    neur_seltype = pd.concat([tot_good, not_sel])
+    
+    return neur_seltype
+
+
+
+
+def osi_calculator(least_pref_ori, pref_ori, responses, dirs):
+    '''This function calculates the orientation selectivity index of a neuron
+    Args:
+    least_pref_ori: float, least preferred orientation of the neuron
+    pref_ori: float, preferred orientation of the neuron
+    responses: array, array of responses of the neuron at each orientation
+    dirs: array, array of orientations
+
+    Returns:
+    osi: float, orientation selectivity index of the neuron
+    '''
+
+    # Extract the activity at the preferred orientation and at the least preferred one
+    maxact = []
+    minact = []
+
+    ind_max = np.where(dirs==pref_ori)[0]
+    ind_min = np.where(dirs==least_pref_ori)[0]
+    maxact.append(responses[ind_max[0]])
+    minact.append(responses[ind_min[0]])
+
+    osi = (maxact-minact)/(maxact+minact)
+    return osi
+
+   
 if __name__ == '__main__':
     import os
     print(os.getcwd())
