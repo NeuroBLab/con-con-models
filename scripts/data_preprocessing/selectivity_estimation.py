6e88c0b600ba77eae39cfe5fc53dee32c75b1ef7
Jacopo Biggiogera <jacopo.biggiogera@yahoo.it>
2024-03-25 11:32:07 +0100

    added executiuon logs print statements

--- scripts/data_preprocessing/selectivity_estimation.py
+++ scripts/data_preprocessing/selectivity_estimation.py
@@ -11,25 +11,25 @@ from standard_transform import minnie_transform_vx
 
 tform_vx = minnie_transform_vx()
 
-
 #Read in data with selectivity infromation
 l234_orifits = pd.read_pickle('../../data/in_processing/orientation_fits.pkl')
 
+print('Assigning selectivity type to neurons')
 # Identify the tuning types of neurons based on pvalue and r squared thresholds
 neur_seltype = tuning_labler(l234_orifits)
 
 # Subset only the neurons in L2/3 and 4 of V1 to get layer, area and position labels
-client = client_version(661)
-func_neurons = subset_v1l234(client, area_df = '../../data_full/v1_n.csv')
+client = client_version(343) #select the CaveDatabase version you are interested in 
+func_neurons = subset_v1l234(client,table_name='functional_coreg', area_df = '../../data_full/v1_n.csv')
 #split column with position values in to three separate columns
 func_neurons[['x_pos', 'y_pos', 'z_pos']] = func_neurons['pial_distances'].apply(lambda x: pd.Series(x))
 
-
 #Merge to get information on layer
-neur_seltype = neur_seltype.merge(func_neurons, left_on=['root_id', 'session', 'scan_idx', 'unit_id'], right_on = ['pt_root_id', 'session', 'scan_idx', 'unit_id'], how = 'inner')
+neur_seltype = neur_seltype.merge(func_neurons, left_on=['root_id', 'session', 'scan_idx', 'cell_id'], right_on = ['pt_root_id', 'session', 'scan_idx', 'unit_id'], how = 'inner')
 
 
 # Calculate the OSI of neurons
+print('Calculating OSI for each neuron')
 #Directions shown during stimulus presentation
 arr_dirs = np.array(sorted(list(set(neur_seltype['orientations'].values[0]))))
 
@@ -46,9 +46,10 @@ for pref_ori, tunig_type, responses in zip(neur_seltype['phi'].values, neur_selt
 neur_seltype['osi'] = osis
 
 #Add indexed version of the preferred angle
-neur_seltype['pref_ori'] = neur_seltype['phi'].apply(angle_indexer, axis = 1)
+neur_seltype['pref_ori'] = neur_seltype['phi'].apply(angle_indexer)
 
 # Add inhibitory neurons and relevant column to identify them
+print('Adding information on inhibitory neurons')
 inhib_neurons = pd.read_pickle('/Users/jacopobiggiogera/Desktop/con-con-models/data/in_processing/inhibitory_nurons_ba.pkl')
 nuc = client.materialize.query_table('aibs_soma_nuc_metamodel_preds_v117')
 
@@ -81,8 +82,11 @@ excit_clean = neur_seltype[['root_id','pref_ori','cell_type',  'tuning_type','os
 neurs_all = pd.concat([excit_clean, inhib_clean], axis = 0)
 
 #Add proofreading information
+print('Adding proofreading information')
 proofread = load_table(client, 'proofreading_status_public_release')
-neurs_all.apply(identify_proofreading_status, axis=1, args = (proofread,'root_id' ))
+neurs_all['proofreading'] = neurs_all.apply(identify_proofreading_status, axis=1, args = (proofread,'root_id' ))
 
 #Save the dataframe
+print('Saving the data')
 neurs_all.to_csv('../../data/preprocessed/unit_table.csv', index = False)
+print('Data saved') 
