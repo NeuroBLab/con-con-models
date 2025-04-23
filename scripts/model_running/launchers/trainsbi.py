0b71f91e23852c5f43f2c61fc2745f7e0788e9b8
Victor Buendia <vbuendiar@gmail.com>
2025-04-23 15:12:02 +0200

    Prepare to do simulations storing all the rates with smaller sizes and so on

--- scripts/model_running/launchers/trainsbi.py
+++ scripts/model_running/launchers/trainsbi.py
@@ -22,32 +22,52 @@ else:
 datafolder = "data"
 
 nfiles = 300
-params = np.empty((0,8))
+params = np.empty((0, nparameters))
 summary_stats = np.empty((0,16)) 
 
 for i in range(nfiles):
+    print(i)
     inputfile = np.loadtxt(f"{datafolder}/model/simulations/{inputfolder}/{i}.txt")
     if inputfile.size > 0:
         params = np.vstack((params, inputfile[:, :nparameters]))
-        summary_stats = np.vstack((summary_stats, inputfile[:, nparameters:]))
+        #Observe that even if mode is NOT kin, the indegree is always stored as a parameter (it's just 400) so the summary stats start always from 9 and does not depend on nparameters
+        summary_stats = np.vstack((summary_stats, inputfile[:, 9:]))
+        
+if sample_mode == 'kin':
+    #Select parameters J,g,sE,sI,hEI,hII,kin, leaving out the two betas
+    params = torch.tensor(params[:, [0,1,2,3,4,5,8]])
+    summary_stats = torch.tensor(summary_stats)
 
-params = torch.tensor(params)
-summary_stats = torch.tensor(summary_stats)
+    #Do the SBI
+    j0, jf = 1., 4.
+    g0, gf = 0., 5.
+    #theta0, thetaf = 19., 19.
+    sigmaE0, sigmaEf = 7., 12.
+    sigmaI0, sigmaIf = 7., 12.
+    hei0, heif = 50., 150.
+    hii0, hiif = 100., 500.
+    kin0, kinf= 30, 600 
 
-#Do the SBI
-j0, jf = 1., 4.
-g0, gf = 0., 5.
-#theta0, thetaf = 19., 19.
-sigmaE0, sigmaEf = 7., 12.
-sigmaI0, sigmaIf = 7., 12.
-hei0, heif = 50., 150.
-hii0, hiif = 100., 500.
-b230, b23f = 0.1, 0.6 
-b40, b4f   = 0.1, 0.6 
+    prior_lowbound =  torch.tensor([j0, g0, sigmaE0, sigmaI0, hei0, hii0, kin0])
+    prior_highbound = torch.tensor([jf, gf, sigmaEf, sigmaIf, heif, hiif, kinf])
+else:
+    params = torch.tensor(params)
+    summary_stats = torch.tensor(summary_stats)
 
-#prior, intervals = msbi.setup_prior(j0, jf, g0, gf, theta0, thetaf, sigma0, sigmaf, hei0, heif, hii0, hiif)
-prior_lowbound =  torch.tensor([j0, g0, sigmaE0, sigmaI0, hei0, hii0, b230, b40])
-prior_highbound = torch.tensor([jf, gf, sigmaEf, sigmaIf, heif, hiif, b23f, b4f])
+    #Do the SBI
+    j0, jf = 1., 4.
+    g0, gf = 0., 5.
+    #theta0, thetaf = 19., 19.
+    sigmaE0, sigmaEf = 7., 12.
+    sigmaI0, sigmaIf = 7., 12.
+    hei0, heif = 50., 150.
+    hii0, hiif = 100., 500.
+    b230, b23f = 0.1, 0.6 
+    b40, b4f   = 0.1, 0.6 
+
+    #prior, intervals = msbi.setup_prior(j0, jf, g0, gf, theta0, thetaf, sigma0, sigmaf, hei0, heif, hii0, hiif)
+    prior_lowbound =  torch.tensor([j0, g0, sigmaE0, sigmaI0, hei0, hii0, b230, b40])
+    prior_highbound = torch.tensor([jf, gf, sigmaEf, sigmaIf, heif, hiif, b23f, b4f])
 
 prior = sbiut.BoxUniform(low=prior_lowbound, high=prior_highbound)
 posterior = msbi.train_sbi(prior, params, summary_stats)
@@ -57,5 +77,5 @@ msbi.save_posterior(f"{datafolder}/model/sbi_networks/{networkname}.sbi", poster
 
 output = open(f'{datafolder}/model/sbi_networks/{networkname}_metadata.txt', 'w')
 gitc = wtm.get_commit_hash()
-output.write("Network generated with commit hash: {gitc} and sample mode {sample_mode}")
+output.write(f"Network generated with commit hash: {gitc} and sample mode {sample_mode}")
 output.close()
